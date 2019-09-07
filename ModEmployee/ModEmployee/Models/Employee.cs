431e2a301e79f514102867af36e150be70f271bf
Jose Gutierrez <josegtzr99@gmail.com>
2019-09-06 21:06:18 -0400

    Primer commit.

---
+++ ModEmployee/ModEmployee/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModEmployee.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Salary { get; set; }
+        [Required]
+        [Range(1, 100000)]
+        public DateTime Birthday { get; set; }
+        [Required]
+        public string Recomendation { get; set; }
+    }
+}
